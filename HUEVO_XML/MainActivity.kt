a32c2e234566baba9bdb5703bf47556a6ae00db4
antiaapardo <pardojimenezantia@gmail.com>
2025-10-08 13:35:22 +0200

    Create MainActivity.kt

---
+++ HUEVO_XML/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.huevoxml
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var contador =0;
+
+        val tvHuevo = findViewById<TextView>(R.id.tvHuevo)
+        val tvContador = findViewById<TextView>(R.id.tvContador)
+
+        tvHuevo.setOnClickListener {
+            contador++
+            tvContador.text = "Taps: $contador"
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
